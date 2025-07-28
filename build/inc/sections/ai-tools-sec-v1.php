46a35cf0637256889bd743eb38652e26b1656fe3
unknown <you@example.com>
2025-07-28 20:47:35 +0600

    Update || Tab Fixing

--- build/inc/sections/ai-tools-sec-v1.php
+++ build/inc/sections/ai-tools-sec-v1.php
@@ -12,104 +12,192 @@ AI-Tools Area
                     </div>
             </div>
         </div>  
-         <div class="row gy-60 flex-row-reverse">
-            <div class="col-xxl-6">
-                <div class="tab-content">
-                    <div class="tab-pane fade show active" id="aitoolTab1" role="tabpanel" aria-labelledby="aitool-tab1">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-1.jpg" alt="">
-                        </div>
+        <!-- <div class="row gy-60 flex-row-reverse">
+        <div class="col-xxl-6">
+            <div class="tab-content">
+                <div class="tab-pane fade show active" id="aitoolTab1" role="tabpanel" aria-labelledby="aitool-tab1">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-1.jpg" alt="">
                     </div>
-                    <div class="tab-pane fade" id="aitoolTab2" role="tabpanel" aria-labelledby="aitool-tab2">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-2.jpg" alt="">
-                        </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab2" role="tabpanel" aria-labelledby="aitool-tab2">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-2.jpg" alt="">
                     </div>
-                    <div class="tab-pane fade" id="aitoolTab3" role="tabpanel" aria-labelledby="aitool-tab3">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-3.jpg" alt="">
-                        </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab3" role="tabpanel" aria-labelledby="aitool-tab3">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-3.jpg" alt="">
                     </div>
-                    <div class="tab-pane fade" id="aitoolTab4" role="tabpanel" aria-labelledby="aitool-tab4">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-4.jpg" alt="">
-                        </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab4" role="tabpanel" aria-labelledby="aitool-tab4">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-4.jpg" alt="">
                     </div>
-                    <div class="tab-pane fade" id="aitoolTab5" role="tabpanel" aria-labelledby="aitool-tab5">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-5.jpg" alt="">
-                        </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab5" role="tabpanel" aria-labelledby="aitool-tab5">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-5.jpg" alt="">
                     </div>
-                    <div class="tab-pane fade" id="aitoolTab6" role="tabpanel" aria-labelledby="aitool-tab6">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-6.jpg" alt="">
-                        </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab6" role="tabpanel" aria-labelledby="aitool-tab6">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-6.jpg" alt="">
                     </div>
-                    <div class="tab-pane fade" id="aitoolTab7" role="tabpanel" aria-labelledby="aitool-tab7">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-7.jpg" alt="">
-                        </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab7" role="tabpanel" aria-labelledby="aitool-tab7">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-7.jpg" alt="">
                     </div>
-                    <div class="tab-pane fade" id="aitoolTab8" role="tabpanel" aria-labelledby="aitool-tab8">
-                        <div class="ai-box-img">
-                        <img src="assets/img/normal/ai-thumb1-8.jpg" alt="">
-                        </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab8" role="tabpanel" aria-labelledby="aitool-tab8">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-8.jpg" alt="">
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="col-xxl-6">
+            <div class="aitool-wrap nav nav-tabs ai-tab" role="tablist">
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link  active" id="aitool-tab1" data-bs-toggle="tab" data-bs-target="#aitoolTab1" type="button" role="tab" aria-controls="aitoolTab1" aria-selected="true">
+                <h2 class="box-title">Background Replace</h2>
+                <p class="box-text">Create unique artwork by simply describing your vision using our AI art generator.</p>
+                </button>
+            </div>
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link" id="aitool-tab2" data-bs-toggle="tab" data-bs-target="#aitoolTab2" type="button" role="tab" aria-controls="aitoolTab2" aria-selected="false">
+                <h2 class="box-title">Text to image</h2>
+                <p class="box-text">Transform your words into stunning AI visuals with our text-to-image feature</p>
+                </button>
+            </div>
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link" id="aitool-tab3" data-bs-toggle="tab" data-bs-target="#aitoolTab3" type="button" role="tab" aria-controls="aitoolTab3" aria-selected="false">
+                <h2 class="box-title">Expand Image</h2>
+                <p class="box-text">Boost the quality of your images with our advanced AI image enhancement tools.</p>
+                </button>
+            </div>
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link" id="aitool-tab4" data-bs-toggle="tab" data-bs-target="#aitoolTab4" type="button" role="tab" aria-controls="aitoolTab4" aria-selected="false">
+                <h2 class="box-title">AI Art Generator</h2>
+                <p class="box-text">Create unique artwork by simply describing your vision using our AI art generator.</p>
+                </button>
+            </div>
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link" id="aitool-tab5" data-bs-toggle="tab" data-bs-target="#aitoolTab5" type="button" role="tab" aria-controls="aitoolTab5" aria-selected="false">
+                <h2 class="box-title">Image Remix</h2>
+                <p class="box-text">Transform your words into stunning AI visuals with our text-to-image feature</p>
+                </button>
+            </div>
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link" id="aitool-tab6" data-bs-toggle="tab" data-bs-target="#aitoolTab6" type="button" role="tab" aria-controls="aitoolTab6" aria-selected="false">
+                <h2 class="box-title">Image Enhancer</h2>
+                <p class="box-text">Boost the quality of your images with our advanced AI image enhancement tools.</p>
+                </button>
+            </div>
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link" id="aitool-tab7" data-bs-toggle="tab" data-bs-target="#aitoolTab7" type="button" role="tab" aria-controls="aitoolTab7" aria-selected="false">
+                <h2 class="box-title">Inpainting</h2>
+                <p class="box-text">Apply artistic style to your photo effortlessly with our innovative style transfer technology.</p>
+                </button>
+            </div>
+            <div class="nav-item" role="presentation">
+                <button class="box-item nav-link" id="aitool-tab8" data-bs-toggle="tab" data-bs-target="#aitoolTab8" type="button" role="tab" aria-controls="aitoolTab8" aria-selected="false">
+                <h2 class="box-title">Style Transfer</h2>
+                <p class="box-text">Apply artistic style to your photo effortlessly with our innovative style transfer technology.</p>
+                </button>
+            </div>
+            </div>
+        </div>
+        </div> -->
+        
+        <!-- Tabs Section -->
+        <div class="row gy-60 flex-row-reverse">
+            <!-- Tab Content (Images) -->
             <div class="col-xxl-6">
-                <div class="aitool-wrap nav nav-tabs ai-tab" role="tablist">
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link  active" id="aitool-tab1" data-bs-toggle="tab" data-bs-target="#aitoolTab1" type="button" role="tab" aria-controls="aitoolTab1" aria-selected="true">
+                <div class="tab-content" id="aiTabContent">
+                <div class="tab-pane fade show active" id="aitoolTab1" role="tabpanel" aria-labelledby="aitool-tab1">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab2" role="tabpanel" aria-labelledby="aitool-tab2">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-2.jpg" alt="">
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab3" role="tabpanel" aria-labelledby="aitool-tab3">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-3.jpg" alt="">
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab4" role="tabpanel" aria-labelledby="aitool-tab4">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-4.jpg" alt="">
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab5" role="tabpanel" aria-labelledby="aitool-tab5">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-5.jpg" alt="">
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab6" role="tabpanel" aria-labelledby="aitool-tab6">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-6.jpg" alt="">
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab7" role="tabpanel" aria-labelledby="aitool-tab7">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-7.jpg" alt="">
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="aitoolTab8" role="tabpanel" aria-labelledby="aitool-tab8">
+                    <div class="ai-box-img">
+                    <img src="assets/img/normal/ai-thumb1-8.jpg" alt="">
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <!-- Tab Buttons (AI Tools) -->
+            <div class="col-xxl-6">
+                <div class="aitool-wrap nav nav-tabs ai-tab" id="aiTab" role="tablist">
+                <button class="nav-link box-item active" id="aitool-tab1" data-bs-toggle="tab" data-bs-target="#aitoolTab1" type="button" role="tab" aria-controls="aitoolTab1" aria-selected="true">
                     <h2 class="box-title">Background Replace</h2>
                     <p class="box-text">Create unique artwork by simply describing your vision using our AI art generator.</p>
-                    </button>
-                </div>
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link" id="aitool-tab2" data-bs-toggle="tab" data-bs-target="#aitoolTab2" type="button" role="tab" aria-controls="aitoolTab2" aria-selected="false">
+                </button>
+                <button class="nav-link box-item" id="aitool-tab2" data-bs-toggle="tab" data-bs-target="#aitoolTab2" type="button" role="tab" aria-controls="aitoolTab2" aria-selected="false">
                     <h2 class="box-title">Text to image</h2>
-                    <p class="box-text">Transform your words into stunning AI visuals with our text-to-image feature</p>
-                    </button>
-                </div>
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link" id="aitool-tab3" data-bs-toggle="tab" data-bs-target="#aitoolTab3" type="button" role="tab" aria-controls="aitoolTab3" aria-selected="false">
+                    <p class="box-text">Transform your words into stunning AI visuals with our text-to-image feature.</p>
+                </button>
+                <button class="nav-link box-item" id="aitool-tab3" data-bs-toggle="tab" data-bs-target="#aitoolTab3" type="button" role="tab" aria-controls="aitoolTab3" aria-selected="false">
                     <h2 class="box-title">Expand Image</h2>
                     <p class="box-text">Boost the quality of your images with our advanced AI image enhancement tools.</p>
-                    </button>
-                </div>
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link" id="aitool-tab4" data-bs-toggle="tab" data-bs-target="#aitoolTab4" type="button" role="tab" aria-controls="aitoolTab4" aria-selected="false">
+                </button>
+                <button class="nav-link box-item" id="aitool-tab4" data-bs-toggle="tab" data-bs-target="#aitoolTab4" type="button" role="tab" aria-controls="aitoolTab4" aria-selected="false">
                     <h2 class="box-title">AI Art Generator</h2>
                     <p class="box-text">Create unique artwork by simply describing your vision using our AI art generator.</p>
-                    </button>
-                </div>
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link" id="aitool-tab5" data-bs-toggle="tab" data-bs-target="#aitoolTab5" type="button" role="tab" aria-controls="aitoolTab5" aria-selected="false">
+                </button>
+                <button class="nav-link box-item" id="aitool-tab5" data-bs-toggle="tab" data-bs-target="#aitoolTab5" type="button" role="tab" aria-controls="aitoolTab5" aria-selected="false">
                     <h2 class="box-title">Image Remix</h2>
-                    <p class="box-text">Transform your words into stunning AI visuals with our text-to-image feature</p>
-                    </button>
-                </div>
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link" id="aitool-tab6" data-bs-toggle="tab" data-bs-target="#aitoolTab6" type="button" role="tab" aria-controls="aitoolTab6" aria-selected="false">
+                    <p class="box-text">Transform your words into stunning AI visuals with our text-to-image feature.</p>
+                </button>
+                <button class="nav-link box-item" id="aitool-tab6" data-bs-toggle="tab" data-bs-target="#aitoolTab6" type="button" role="tab" aria-controls="aitoolTab6" aria-selected="false">
                     <h2 class="box-title">Image Enhancer</h2>
                     <p class="box-text">Boost the quality of your images with our advanced AI image enhancement tools.</p>
-                    </button>
-                </div>
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link" id="aitool-tab7" data-bs-toggle="tab" data-bs-target="#aitoolTab7" type="button" role="tab" aria-controls="aitoolTab7" aria-selected="false">
+                </button>
+                <button class="nav-link box-item" id="aitool-tab7" data-bs-toggle="tab" data-bs-target="#aitoolTab7" type="button" role="tab" aria-controls="aitoolTab7" aria-selected="false">
                     <h2 class="box-title">Inpainting</h2>
                     <p class="box-text">Apply artistic style to your photo effortlessly with our innovative style transfer technology.</p>
-                    </button>
-                </div>
-                <div class="nav-item" role="presentation">
-                    <button class="box-item nav-link" id="aitool-tab8" data-bs-toggle="tab" data-bs-target="#aitoolTab8" type="button" role="tab" aria-controls="aitoolTab8" aria-selected="false">
+                </button>
+                <button class="nav-link box-item" id="aitool-tab8" data-bs-toggle="tab" data-bs-target="#aitoolTab8" type="button" role="tab" aria-controls="aitoolTab8" aria-selected="false">
                     <h2 class="box-title">Style Transfer</h2>
                     <p class="box-text">Apply artistic style to your photo effortlessly with our innovative style transfer technology.</p>
-                    </button>
-                </div>
+                </button>
                 </div>
             </div>
-         </div>
+        </div>
+
     </div>
    
 </section>
